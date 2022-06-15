2dc4f8797824f67503f8bcec3940887fea962824
mouz <chintienyong@gmail.com>
2022-06-15 12:06:10 +0800

    week2 exercise

---
+++ week2/exercise1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+
+    <?php
+    $surname = "mouz";
+    $given = "cty";
+    $age = "18";
+    echo "My name is <b> $surname $given </b>";
+    echo "<br>";
+    echo "I am " . $age . " " . "years old";
+    ?>
+
+</body>
+
+</html>
